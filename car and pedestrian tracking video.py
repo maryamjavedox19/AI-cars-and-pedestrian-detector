c73e2afca03315a7535723e06a508f5b95a7b657
Maryam <maryamjaved.xo@gmail.com>
2023-08-26 23:14:39 +0500

    detecting cars in a video

--- car and pedestrian tracking video.py
+++ car and pedestrian tracking video.py
@@ -1,7 +1,12 @@
 import cv2
+
 video_file='Cars and Pedestrians_Trim.mp4'
 
 video=cv2.VideoCapture(video_file)
+
+classifier_file='cars.xml'
+
+car_tracker=cv2.CascadeClassifier(classifier_file)
 
 
 while True:
@@ -9,6 +14,14 @@ while True:
 
     if not success:
         break
+
+    # grayscale video
+    grayscaled_frame=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cars_coordinates=car_tracker.detectMultiScale(grayscaled_frame)
+
+    for (x,y,w,h) in cars_coordinates:
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2)
 
     cv2.imshow('showing video', frame)
     key=cv2.waitKey(1)
